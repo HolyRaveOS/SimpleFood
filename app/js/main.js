69a3d94b083e9c94e68f8922144250dd5267a03d
HolyRaveOS <kitlaruk1995@gmail.com>
2023-04-25 15:25:13 +0300

    added

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,9 @@ $(function () {
 	var mixer = mixitup('.popular__list');
 
 	$(document).ready(function () {
+		$('.header__user-form').hide();
 		$('.header__user-btn-cart').click(function () {
 			$('.header__cart-content').slideToggle('fast');
-			$('.header__cart-content').toggleClass('show');
 		});
 		$('.header__user-form').css('display', 'flex').hide();
 		$('.header__user-btn').click(function () {
