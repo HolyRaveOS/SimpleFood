87bf26ac7859680a8d917e3f47e4c7973ceb3f05
HolyRaveOS <kitlaruk1995@gmail.com>
2023-04-26 19:29:34 +0300

    added stay, restaurants, reviews, app, section

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,6 @@
 
 $(function () {
 	var mixer = mixitup('.popular__list');
-
 	$(document).ready(function () {
 		$('.header__user-form').hide();
 		$('.header__user-btn-cart').click(function () {
@@ -12,7 +11,80 @@ $(function () {
 			$('.header__user-form').slideToggle('fast');
 		});
 	});
+
+
+	$(document).ready(function () {
+
+		const swiper = new Swiper('.restaurants__slider', {
+			loop: true,
+			pagination: {
+				el: '.restaurants__pagination',
+				clickable: true,
+			},
+			breakpoints: {
+				992: {
+
+				},
+			}
+		});
+		if (window.innerWidth > 992) {
+			swiper.destroy();
+		}
+	});
+
+	$(document).ready(function () {
+		var $window = $(window);
+		var $swiperContainer = $('.restaurants__slider');
+		var swiper;
+
+		function initSwiper() {
+			swiper = new Swiper($swiperContainer, {
+			});
+		}
+
+		function disableSwiper() {
+			if (swiper) {
+				swiper.destroy();
+				swiper = undefined;
+			}
+		}
+		function checkWindowSize() {
+			if ($window.width() <= 992) {
+				if (!swiper) {
+					initSwiper();
+				}
+			} else {
+				disableSwiper();
+			}
+		}
+
+		checkWindowSize();
+		$window.on('resize', checkWindowSize);
+	});
+
+	$(document).ready(function () {
+		const swiper2 = new Swiper('.reviews__slider', {
+			loop: true,
+			effect: 'flip',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			pagination: {
+				el: '.reviews__pagination',
+				clickable: true,
+			},
+			navigation: {
+				nextEl: '.reviews__arrow-right',
+				prevEl: '.reviews__arrow-left',
+			},
+		});
+	});
 });
+
+
+
+
 
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,7 @@ function scripts() {
 	return src([
 		'node_modules/jquery/dist/jquery.js',
 		'node_modules/mixitup/dist/mixitup.min.js',
+		'node_modules/swiper/swiper-bundle.min.js',
 		'app/js/main.js'
 	])
 		.pipe(concat('main.min.js'))
