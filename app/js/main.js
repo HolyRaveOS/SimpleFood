09d5a68457c11e1b45da4b25eda0d30236247a56
HolyRaveOS <kitlaruk1995@gmail.com>
2023-05-04 20:10:24 +0300

    finished task #14

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,13 @@
 
 $(function () {
 
-
 	$(window).resize(function () {
-		if ($(window).width() < 800) {
-			$(".restaurants__wrapper").addClass("swiper");
+		if ($(window).width() < 1200) {
+			$(".popular__wrapper").addClass("swiper");
+			$(".popular__filter-item").addClass("swiper-slide");
 		} else {
-			$(".restaurants__wrapper").removeClass("swiper");
+			$(".popular__wrapper").removeClass("swiper");
+			$(".popular__filter-item").removeClass("swiper-slide");
 		}
 	});
 
@@ -14,14 +15,11 @@ $(function () {
 		var mySwiper = null;
 
 		function initSwiper() {
-			if ($(window).width() <= 800) {
+			if ($(window).width() <= 1200) {
 				if (!mySwiper) {
-					mySwiper = new Swiper('.restaurants__wrapper', {
-						loop: true,
-						pagination: {
-							el: '.restaurants__pagination',
-							clickable: true,
-						},
+					mySwiper = new Swiper('.popular__wrapper', {
+						loop: false,
+						slidesPerView: "auto",
 					});
 				}
 			} else {
@@ -39,9 +37,76 @@ $(function () {
 		});
 	});
 
-	var mixer = mixitup('.popular__list');
+
+
+
 	$(document).ready(function () {
-		$('.header__search-form').hide();
+		$('.burger-open,.burger-close,.menu__list a').click(function (event) {
+			$('.burger-close,.burger-close__icon,.mobile').toggleClass('active')
+			$('body').toggleClass('lock');
+			event.stopPropagation();
+		});
+
+		$(document).click(function (event) {
+			var container = $(".mobile");
+			if (!container.is(event.target) && container.has(event.target).length === 0) {
+				container.removeClass('active');
+				$('body').removeClass('lock');
+			}
+			var screenWidth = $(window).width();
+			var rightOffset = screenWidth - (screenWidth * 0.21);
+			if (event.pageX > rightOffset) {
+				container.removeClass('active');
+				$('body').removeClass('lock');
+			}
+		});
+	});
+
+
+	$(window).resize(function () {
+		if ($(window).width() < 768) {
+			$(".restaurants__wrapper").addClass("swiper");
+			$(".restaurants__item").addClass("swiper-slide");
+		} else {
+			$(".restaurants__wrapper").removeClass("swiper");
+			$(".restaurants__item").removeClass("swiper-slide");
+		}
+	});
+
+	$(document).ready(function () {
+		var mySwiper = null;
+
+		function initSwiper() {
+			if ($(window).width() <= 768) {
+				if (!mySwiper) {
+					mySwiper = new Swiper('.restaurants__wrapper', {
+						pagination: {
+							el: '.restaurants__pagination',
+							clickable: true,
+						},
+						loop: false,
+						slidesPerView: "auto",
+						spaceBetween: 20,
+					});
+				}
+			} else {
+				if (mySwiper) {
+					mySwiper.destroy();
+					mySwiper = null;
+				}
+			}
+		}
+
+		initSwiper();
+
+		$(window).resize(function () {
+			initSwiper();
+		});
+	});
+
+
+	$(document).ready(function () {
+		$('.header__search-form,.header__cart').hide();
 		$('.header__user-btn-cart').click(function () {
 			$('.header__cart').slideToggle('fast');
 		});
@@ -54,7 +119,7 @@ $(function () {
 
 
 	$(document).ready(function () {
-		const swiper2 = new Swiper('.reviews__content', {
+		const swiper3 = new Swiper('.reviews__content', {
 			loop: true,
 			effect: 'flip',
 			autoplay: {
@@ -71,7 +136,14 @@ $(function () {
 			},
 		});
 	});
+
+
+	var mixer = mixitup('.popular__list');
+
 });
+
+
+
 
 
 
